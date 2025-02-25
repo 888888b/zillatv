bd89e370534210d6e4dba5bc6a5afbe7a40bb0d0
Vitor Hugo <Hugosilva55442@gmail.com>
2025-02-25 13:10:30 -0300

    feat: add global constants file to store static values

---
+++ src/app/constants.ts
@@ -0,0 +1,21 @@
+export const tmdbConfig = {
+    high_resolution_poster: 'https://image.tmdb.org/t/p/original',
+    high_resolution_backdrop: 'https://image.tmdb.org/t/p/original',
+    low_resolution_poster: 'https://image.tmdb.org/t/p/w500',
+    low_resolution_backdrop: 'https://image.tmdb.org/t/p/w780'
+};
+
+export const tmdbGenres = {
+    release: { genre: 'release', title: 'Em Destaque: Os Filmes Mais Recentes' },
+    horror: { genre: '27', title: 'Horror em exibição' },
+    action: { genre: '28', title: 'Adrenalina em cartaz' },
+    comedy: { genre: '35', title: 'Comédia: Diversão com a família' },
+    cartoon: { genre: '16', title: 'Diversão para Crianças' },
+    romance: { genre: '10749', title: 'Histórias de Amor à Moda Antiga' },
+    documentary: { genre: '99', title: 'Documentando o mundo' },
+    war: { genre: '10752', title: 'Guerra: Uma Batalha pela Sobrevivência' },
+    fiction: { genre: '878', title: 'Universos paralelos: Ficção' },
+    adventure: { genre: '12', title: 'Desbravando o desconhecido: Aventuras' }
+};
+
+
