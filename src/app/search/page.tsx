29d9bf517165c4e9fa37107b5e4815d4ad9afc64
Vitor Hugo <Hugosilva55442@gmail.com>
2025-03-01 20:19:02 -0300

    feat(search page): add loading animation

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,8 +1,20 @@
 import SearchPage from "@/components/templates/searchPage"
+import { Suspense } from "react";
 
 type SearchProps = { keyword: string | undefined }
 
 export default async function Search({ searchParams }:{ searchParams: Promise<SearchProps> }) {
+
     const { keyword } = await searchParams;
-    return <SearchPage keyword={keyword}/>
+    const loadingAnimation = (
+        <div className="w-screen h-screen fixed top-0 left-0 bg-richblack flex items-center justify-center">
+          <span className="loading loading-spinner loading-lg bg-white/80"></span>
+        </div>
+    );
+
+    return (
+        <Suspense fallback={loadingAnimation}>
+            <SearchPage keyword={keyword}/>
+        </Suspense>
+    );
 };
