601078d369ed7b7f436c6746e7949d787534b240
Vitor Hugo <Hugosilva55442@gmail.com>
2025-03-10 08:43:00 -0300

    style: change the color of the navigation arrows in the movie and series carousels

--- src/components/molecules/emblaDefaultNavigation/index.tsx
+++ src/components/molecules/emblaDefaultNavigation/index.tsx
@@ -6,32 +6,38 @@ const EmblaNavigation = memo(( props: UsePrevNextButtonsType ) => {
     return (
         <>
             {/* Botão para o slide anterior */ }
-            <button 
-                onClick={props.onPrevButtonClick}
-                className={`absolute left-0 top-[260px] -translate-y-[140px] z-50 w-[45px] h-[45px] rounded-full -translate-x-1/2 bg-orange-800 hover:bg-orangered cursor-pointer embla-navigation`}
-                style={{ 
-                    opacity: props.prevBtnDisabled ? '0' : '100%', 
+            <div 
+                className="w-16 h-full absolute left-0 top-0 z-50 flex items-center justify-start bg-gradient-to-l from-transparent to-deepnight embla-navigation"
+                style={{
+                    opacity: props.prevBtnDisabled ? '0' : '100%',
                     pointerEvents: props.prevBtnDisabled ? 'none' : 'auto'
-                }}
-                >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.8} stroke="currentColor" className="size-6 text-white">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-                </svg>
-            </button>
+                }}>
+                <button
+                    onClick={props.onPrevButtonClick}
+                    className={`w-8 h-8 flex items-center justify-center rounded-full bg-white hover:bg-white cursor-pointer -translate-x-8`}
+                    >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.8} stroke="currentColor" className="size-5 text-black">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                    </svg>
+                </button>
+            </div>
 
             {/* Botão para o proximo slide */ }
-            <button 
-                onClick={props.onNextButtonClick}
-                className={`absolute right-0 top-[260px] -translate-y-[140px] z-50 w-[45px] h-[45px] rounded-full translate-x-1/2 bg-orange-800 hover:bg-orangered cursor-pointer embla-navigation`}
-                style={{ 
+            <div 
+                className="w-16 h-full absolute right-0 top-0 z-50 flex items-center justify-end bg-gradient-to-r from-transparent to-deepnight embla-navigation"
+                style={{
                     opacity: props.nextBtnDisabled ? '0' : '100%',
                     pointerEvents: props.nextBtnDisabled ? 'none' : 'auto'
-                }}
-                >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.8} stroke="currentColor" className="size-6 text-white">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                </svg>
-            </button>
+                }}>
+                <button
+                    onClick={props.onNextButtonClick}
+                    className={`w-8 h-8 rounded-full bg-white hover:bg-white cursor-pointer embla-navigation translate-x-8`}
+                    >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.8} stroke="currentColor" className="size-5 text-black">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                    </svg>
+                </button>
+            </div>
         </>
     );
 });
