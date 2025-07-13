59cddfb1b6d723ea1bcfac29cc1ec403228da6d4
Vitor Hugo <Hugosilva55442@gmail.com>
2025-07-13 10:47:30 -0300

    style(header-carousel): change the font size of the details button, change the animation time of the title and description of the slides

--- src/components/molecules/detailsButton/index.tsx
+++ src/components/molecules/detailsButton/index.tsx
@@ -7,8 +7,8 @@ const FurtherDetailsButton = memo(( props: ComponentPropsWithRef<'button'> ) => 
     return (
         <button
             {...rest}
-            className={`pointer-events-auto outline-none border-none w-full max-w-[500px] h-12 rounded-[10px] uppercase bg-primary text-primary-content text-sm lg:text-base font-bold sm:w-48 lg:w-52 active:scale-95 transition-transform duration-200 flex items-center gap-x-3 justify-center cursor-pointer ${className}`}>
-            <EyeIcon className="w-[23px] h-[23px] lg:w-[25px] lg:h-[25px] stroke-2 lg:stroke-[2.3]"/>
+            className={`pointer-events-auto outline-none border-none w-full max-w-[500px] h-12 rounded-[10px] uppercase bg-primary text-primary-content text-sm lg:text-[15px] font-bold sm:w-48 lg:w-52 active:scale-95 transition-transform duration-200 flex items-center gap-x-3 justify-center cursor-pointer ${className}`}>
+            <EyeIcon className="w-[23px] h-[23px] lg:w-[24px] lg:h-[24px] stroke-2 lg:stroke-[2.2]"/>
             Ver detalhes
         </button>
     );
