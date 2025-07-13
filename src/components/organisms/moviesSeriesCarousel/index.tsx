2830f12a6d3e73b92e6fe69f1abc89390d4bd726
Vitor Hugo <Hugosilva55442@gmail.com>
2025-07-13 12:48:38 -0300

    refactor(movies-series-carousel): refactor functions and adapt component to receive new slide types

--- src/components/organisms/moviesSeriesCarousel/index.tsx
+++ src/components/organisms/moviesSeriesCarousel/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 // hooks
-import { useContext } from 'react';
+import { useContext, useCallback } from 'react';
 import { useRouter } from 'next/navigation';
 import useFirebase from "@/components/hooks/firebase";
 
@@ -24,111 +24,125 @@ import { tmdbConfig } from '@/app/constants';
 import './styles.css';
 
 type ComponentProps = {
-    contentData: tmdbObjProps[] | undefined
-    contentType: 'movie' | 'serie'
+    slidesData: tmdbObjProps[] | undefined;
+    slidesType: 'movie' | 'serie' | 'mixed';
 };
 
-export default function MoviesSeriesCarousel( props: ComponentProps ) {
+export default function MoviesSeriesCarousel(props: ComponentProps) {
 
-    const router = useRouter(); 
-    const { dispatch } = useContext( GlobalEventsContext );
+    const { push } = useRouter();
+    const { dispatch } = useContext(GlobalEventsContext);
     const {
         low_resolution_poster,
         low_resolution_backdrop
     } = tmdbConfig;
 
-    const { contentType } = props;
-    const { 
-        addUserFavoritesToDb, 
-        deleteUserFavoritesOnDb 
+    const { slidesType } = props;
+    const {
+        addUserFavoritesToDb,
+        deleteUserFavoritesOnDb
     } = useFirebase();
-    
-    const { 
-        favoriteMovies, 
-        favoriteSeries, 
-        isLoggedIn 
-    } = useContext( UserDataContext );
+
+    const {
+        favoriteMovies,
+        favoriteSeries,
+        isLoggedIn
+    } = useContext(UserDataContext);
 
     // Define se o filme/serie e favorito ou nao, caso seja, salva no banco de dados
-    const updateUserFavorites = async ( contentId: string ) => {
-        if ( isLoggedIn ) {
-            if (!favoriteMovies?.includes(contentId) && !favoriteSeries?.includes(contentId)) {
-                addUserFavoritesToDb( contentId, contentType );
-                return;
-            };
-            
-            deleteUserFavoritesOnDb( contentId, contentType );
+    const updateUserFavorites = useCallback(async (slideId: string, slideType: string | undefined ): Promise<void> => {
+        // abre o modal de registro caso o usuario nao esteja logado para completar a ação
+        if (!isLoggedIn) {
+            dispatch({ type: 'IS_REGISTER_MODAL_ACTIVE', payload: true });
+            dispatch({
+                type: 'SET_ERROR', payload: {
+                    type: 'formInstructions',
+                    message: 'Faça login ou crie uma conta para adicionar filmes e series aos seus favoritos'
+                }
+            });
+            return;
+        };
+
+        // verifica se o conteudo ja esta dentre os favoritos
+        const isAlreadyFavorite = favoriteSeries?.includes(slideId) || favoriteMovies?.includes(slideId);
+        const type = slidesType === 'mixed' ? slideType : slidesType;
+        if (!type) return;
+
+        if (!isAlreadyFavorite) {
+            addUserFavoritesToDb(slideId, type);
+        } else {
+            deleteUserFavoritesOnDb(slideId, type);
+        };
+
+    }, [ slidesType, dispatch, favoriteMovies, favoriteSeries, isLoggedIn ]);
+
+    // verifica se o conteudo esta nos favorites e retorna true ou false
+    const findFavoriteOnList = useCallback((id: string): boolean => {
+        const isFavorite =
+            favoriteMovies?.includes(id) ||
+                favoriteSeries?.includes(id) ? true : false;
+        return isFavorite;
+    }, [ favoriteMovies, favoriteSeries ]);
+
+    // lida com a navegaçao
+    const navigate = useCallback(( slideId: string, slideType: string | undefined ): void => {
+        if ( slidesType === 'mixed' && slideType ) {
+            push(`/player/${slideType}/${slideId}`, { scroll: true })
             return;
         }; 
 
-        dispatch({type: 'IS_REGISTER_MODAL_ACTIVE', payload: true});
-            
-        dispatch({type: 'SET_ERROR', payload: {
-            type: 'formInstructions',
-            message: 'Faça login ou crie uma conta para adicionar filmes e series aos seus favoritos'
-        }});
-    };
+        push(`/player/${slidesType}/${slideId}`, { scroll: true })
+    }, [ slidesType ]);
 
-    return props.contentData ? (             
+    return props.slidesData ? (
         <div className='movie-serie-carousel'>
             <EmblaCarousel navigationType='default' dragFree={true}>
                 {/* Gerando slides a partir de um array de objetos retornados pela api do TMDB */}
-                { props.contentData.map((content) => (  
-                    content.poster_path || content.backdrop_path ? (
-                        <div className='embla__slide' key={`main-slides-${content.id}`}>  
+                {props.slidesData.map((slide) => (
+                    slide.poster_path || slide.backdrop_path ? (
+                        <div className='embla__slide' key={`main-slides-${slide.id}`}>
                             <>
                                 <div className='slide-image-container'>
                                     {/* Opção para adicionar o filme/serie aos favoritos */}
                                     <FavoriteButton
                                         updateFavorites={updateUserFavorites}
-                                        buttonId={content.id}
-                                        isFavorite={favoriteMovies?.includes(content.id) || favoriteSeries?.includes(content.id) ? true : false}
+                                        buttonId={slide.id}
+                                        isFavorite={findFavoriteOnList(slide.id)}
                                     />
-                                    
+
                                     {/* botao de play */}
-                                    <div 
-                                        className='play-icon-box' 
-                                        onClick={() => router.push(`/player/${contentType}/${content.id}`, {scroll: true})} >
+                                    <div
+                                        className='play-icon-box'
+                                        onClick={() => navigate(slide.id, slide.media_type)} >
                                         <FaPlay className="text-primary-content text-lg translate-x-px" />
                                     </div>
 
                                     {/* Imagem do conteudo a ser exibido */}
-                                    <div 
-                                        className="scale-animation" 
-                                        onClick={() => router.push(`/player/${contentType}/${content.id}`, {scroll: true})}>
+                                    <div
+                                        className="scale-animation"
+                                        onClick={() => navigate(slide.id, slide.media_type)}>
                                         <img
                                             src={
-                                                content.poster_path ? 
-                                                `${low_resolution_poster}${content.poster_path}` : 
-                                                `${low_resolution_backdrop}${content.backdrop_path}`
+                                                slide.poster_path ?
+                                                    `${low_resolution_poster}${slide.poster_path}` :
+                                                    `${low_resolution_backdrop}${slide.backdrop_path}`
                                             }
                                             alt={`
-                                                ${content.title ?? content.name} ${contentType} presentation image`
+                                            ${slide.title ?? slide.name} ${slidesType} presentation image`
                                             }
+                                            loading='lazy'
                                             className="image min-w-full min-h-full object-cover"
                                         />
                                     </div>
                                 </div>
-        
-                                {/* Container de informações sobre o conteudo */}
-                                <div className="mt-[10px] relative pr-2 max-w-[140px] md:max-w-[200px] xl:max-w-56 lg:hidden">      
-                                    {/* Titulo */}
-                                    <h3 
-                                        className="font-medium text-base text-secondary line-clamp-1">
-                                        {content.title ?? content.name}
-                                    </h3>
 
-                                    {/* <div 
-                                        className="flex items-center gap-x-3 font-normal text-neutral-400 text-[15px] md:text-base">
-                                        Data de lançamento
-                                        <p>
-                                            {getReleaseDate(content.release_date ?? content.first_air_date)}
-                                        </p>
-                                        Nota do publico ao conteudo
-                                        <p className="flex items-center gap-x-1 text-primary">
-                                            {(content.vote_average).toFixed(0)}/10
-                                        </p>
-                                    </div>  */}
+                                {/* Container de informações sobre o conteudo */}
+                                <div className="mt-[10px] relative pr-2 max-w-[140px] md:max-w-[200px] xl:max-w-56 lg:hidden">
+                                    {/* Titulo */}
+                                    <h3
+                                        className="font-medium text-base text-secondary/90 line-clamp-1">
+                                        {slide.title ?? slide.name}
+                                    </h3>
                                 </div>
                             </>
                         </div>
