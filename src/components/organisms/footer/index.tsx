ba5620e197fd01786fefd9bd216fcf9662259fe5
Vitor Hugo <Hugosilva55442@gmail.com>
2025-07-17 09:35:10 -0300

    feat(footer): apply new design

--- src/components/organisms/footer/index.tsx
+++ src/components/organisms/footer/index.tsx
@@ -1,29 +1,102 @@
-// Icones com React-icons
-import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
-
-import Link from "next/link";
+import './styles.css';
 
 export default function Footer() {
     return (
-        <div className="flex w-full h-28 box-border gap-x-10 gap-y-5 flex-wrap-reverse bg-darkpurple justify-between items-center px-4 py-5 sm:flex-nowrap sm:py-6 sm:h-20 md:px-6 lg:px-8 ease-linear duration-200 font-raleway">
-            <p className="text-xl font-medium order-1 whitespace-nowrap">Vitor Hugo</p>
-            <div className="flex gap-x-5 items-center *:text-2xl order-2  sm:*:text-xl">
-                {/* Meu linkedin */}
-                <Link target="_blank" href={'https://www.linkedin.com/in/vitor-araujo-2054622a6'} rel="Link para o linkedin de vitor-araujo-2054622a6">
-                    <FaLinkedin className="md:cursor-pointer"/>
-                </Link>
+        <div className="px-4 sm:px-10 lg:px-[70px] mt-10 py-14 bg-surface">
+            <h2 className="text-primary font-black text-[22px] font-raleway">ZilluTV</h2>
+            <div className="bg-secondary/5 h-px w-full max-w-48 mt-[15px]" />
 
-                {/* Meu github */}
-                <Link target="_blank" href={'https://www.github.com/888888b'} rel="Link para o github de 888888b">
-                    <FaGithub className="md:cursor-pointer"/>
-                </Link>
+            <div className="flex flex-wrap gap-y-20 gap-x-[60px] md:gap-20 items-start lg:justify-between mt-10">
+                <div>
+                    <h3 className="font-bold uppercase text-base text-secondary">Conta</h3>
+                    <div className="mt-5">
+                        <ul className="font-normal flex flex-col gap-y-[10px] text-base">
+                            <li><a href="">Criar conta</a></li>
+                            <li><a href="">Entrar</a></li>
+                            <li><a href="">Assinatura</a></li>
+                            <li><a href="">Dispositivos compatíveis</a></li>
+                        </ul>
+                    </div>
+                </div>
 
-                {/* Meu instagram */}
-                <Link target="_blank" href={'https://www.instagram.com/vitor_araujo621'} rel="Link para o instrgam de Vitor_araujo621">
-                    <FaInstagram className="md:cursor-pointer"/>
-                </Link>
+                <div>
+                    <h3 className="font-bold uppercase text-base text-secondary">Sobre</h3>
+                    <div className="mt-5">
+                        <ul className="font-normal flex flex-col gap-y-[10px] text-base">
+                            <li><a href="">Quem Somos</a></li>
+                            <li><a href="">Parceiros</a></li>
+                            <li><a href="">Trabalhe conosco</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div>
+                    <h3 className="font-bold uppercase text-base text-secondary">Ajuda</h3>
+                    <div className="mt-5">
+                        <ul className="font-normal flex flex-col gap-y-[10px] text-base">
+                            <li><a href="">Central de ajuda</a></li>
+                            <li><a href="">Fale conosco</a></li>
+                            <li><a href="">Status do serviço</a></li>
+                            <li><a href="">Perguntas frequentes</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div>
+                    <h3 className="font-bold uppercase text-base text-secondary">Legal</h3>
+                    <div className="mt-5">
+                        <ul className="font-normal flex flex-col gap-y-[10px] text-base">
+                            <li><a href="">Termos de uso</a></li>
+                            <li><a href="">Política de Privacidade</a></li>
+                            <li><a href="">Política de Cookies</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div>
+                    <h3 className="font-bold uppercase text-base text-secondary">Nossas redes</h3>
+                    <div className="mt-5">
+                        <ul className="font-normal flex flex-col gap-y-[10px] text-base">
+                            <li><a href="">Instagram</a></li>
+                            <li><a href="">Twitter/X</a></li>
+                            <li><a href="">Facebook</a></li>
+                            <li><a href="">YouTube</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <p className="text-xl font-medium order-3 w-full text-right sm:w-fit">Zillu TV</p>
+
+            <div className="bg-secondary/5 h-px w-full my-10" />
+
+            <div className="flex items-center flex-col gap-10">
+                <div className='flex flex-wrap gap-5 justify-center items-center'>
+                    {/* icone do google play */}
+                    <img
+                        src={'/google_play_badge.png'}
+                        alt='Google Play badge'
+                        className='h-10 w-auto cursor-pointer'
+                        loading='lazy'
+                    />
+
+                    {/* icon da apple store */}
+                    <img
+                        src={'/apple_store_badge.png'}
+                        alt='Apple store badge'
+                        className='h-11 w-auto cursor-pointer'
+                        loading='lazy'
+                    />
+
+                     {/* icon da microsoft store */}
+                    <img
+                        src={'/microsoft_store_badge.svg'}
+                        alt='Microsoft store badge'
+                        className='h-10 w-auto cursor-pointer border border-secondary/65 rounded-md'
+                        loading='lazy'
+                    />
+                </div>
+
+                <p className="text-base font-semibold">© 2025 ZilluTV - Todos os direitos reservados</p>
+            </div>
         </div>
     );
 };
