b3f10b1648988fcd76ae78d9879f08176bbf565b
Vitor Hugo <Hugosilva55442@gmail.com>
2025-07-24 20:43:05 -0300

    refactor(constants): add titles and genres for series

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,27 +1,41 @@
+// todos os tamanhos para imagens
 export const tmdbConfig = {
-    high_resolution_poster: 'https://image.tmdb.org/t/p/original',
-    high_resolution_backdrop: 'https://image.tmdb.org/t/p/original',
-    medium_resolution_poster: 'https://image.tmdb.org/t/p/w780',
-    medium_resolution_backdrop: 'https://image.tmdb.org/t/p/w1280',
-    low_resolution_poster: 'https://image.tmdb.org/t/p/w342',
-    low_resolution_backdrop: 'https://image.tmdb.org/t/p/w780',
-    profile_photo_base_url: 'https://image.tmdb.org/t/p/w185',
-    still_image_base_url: 'https://image.tmdb.org/t/p/original',
+  high_resolution_poster: 'https://image.tmdb.org/t/p/original',
+  high_resolution_backdrop: 'https://image.tmdb.org/t/p/original',
+  medium_resolution_poster: 'https://image.tmdb.org/t/p/w780',
+  medium_resolution_backdrop: 'https://image.tmdb.org/t/p/w1280',
+  low_resolution_poster: 'https://image.tmdb.org/t/p/w342',
+  low_resolution_backdrop: 'https://image.tmdb.org/t/p/w780',
+  profile_photo_base_url: 'https://image.tmdb.org/t/p/w185',
+  still_image_base_url: 'https://image.tmdb.org/t/p/original',
 };
 
+// generos e respectivos titulos para filmes
 export const tmdbGenres = {
-  release: { genre: 'release', title: 'Em destaque: os filmes mais recentes' },
+  // trending: { genre:'trending', title: 'Em alta' },
+  release: { genre: 'release', title: 'Lançamentos' },
   horror: { genre: '27', title: 'Terror' },
   action: { genre: '28', title: 'Ação' },
   comedy: { genre: '35', title: 'Comédia' },
   cartoon: { genre: '16', title: 'Desenho' },
-  romance: { genre: '10749', title: 'Histórias de amor à moda antiga' },
+  romance: { genre: '10749', title: 'Romance' },
   documentary: { genre: '99', title: 'Documentario' },
-  war: { genre: '10752', title: 'Guerra: uma batalha pela sobrevivência' },
+  war: { genre: '10752', title: 'Guerra' },
   fiction: { genre: '878', title: 'Ficção' },
-  adventure: { genre: '12', title: 'Desbravando o desconhecido: aventuras' },
-  trending: { title: 'Em alta' },
-  popular: { title: 'Séries populares' }
+  adventure: { genre: '12', title: 'Aventura' },
+  // popular: { genre:'popular', title: 'Populares' }
 };
+
+// generos e respectivos titulos para series
+export const tmdbSerieGenres = {
+  release: { genre: 'release', title: 'Lançamentos', },
+  suspense: { genre: '9648', title: 'Suspense', },
+  action: { genre: '10759', title: 'Ação', },
+  comedy: { genre: '35', title: 'Comedia', },
+  war: { genre: '10768', title: 'Guerra', },
+  cartoon: { genre: '16', title: 'Desenho', },
+  documentary: { genre: '99', title: 'Documentario', },
+};
+
 
 
