5da77abb41f4a3c6e903d17678a8a2eb322469bf
Vitor Hugo <Hugosilva55442@gmail.com>
2025-07-30 05:12:38 -0300

    feat(header-carousel): replace movie titles with logos in carousel

--- src/components/organisms/headerCarousel/index.tsx
+++ src/components/organisms/headerCarousel/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 // hooks
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
 // componentes
@@ -9,12 +9,16 @@ import EmblaCarousel from '@/components/organisms/emblaSlides';
 import FurtherDetailsButton from '@/components/molecules/detailsButton';
 import Title from './title';
 import DetailsBar from './details';
-import Image from './image';
+import dynamic from 'next/dynamic';
+const SlideImage = dynamic(() => import('./image/index'), { ssr: true });
 
 import './styles.css';
 
 // tipos
 import { tmdbObjProps } from '@/contexts/tmdbContext';
+
+import { tmdbConfig } from '@/app/constants';
+import { boolean } from 'zod';
 
 type HeaderCarouselProps = {
     currentPage: string
@@ -22,60 +26,114 @@ type HeaderCarouselProps = {
     slidesData: tmdbObjProps[] | undefined
 };
 
-export default function HeaderCarousel( props: HeaderCarouselProps ) {
+type path = {
+    path: string;
+    slide: string;
+};
 
+export default function HeaderCarousel(props: HeaderCarouselProps) {
+
+    const { ImageBasePath } = tmdbConfig;
     const { push } = useRouter();
     const { slidesData, slidesType, currentPage } = props;
-    const [ currentSlide, setCurrentSlide ] = useState<tmdbObjProps | null>(null);
-    const [ activeSlides, setActiveSlides ] = useState<number[]>([]);
+    const [currentSlide, setCurrentSlide] = useState<tmdbObjProps | null>(null);
+    const [activeSlides, setActiveSlides] = useState<number[]>([]);
+    const [logos, setLogos] = useState<path[] | null>(null);
 
     // lida com a nevegaçao entre paginas
-    const navigate = useCallback(( slideId: string ) => {
+    const navigate = useCallback((slideId: string) => {
         push(`/player/${slidesType}/${slideId}`, {
             scroll: true
         });
     }, [slidesType, push]);
 
+    // filtra a lista de imagens de logo buscando por uma imagem em portugues (caso houver logos disponiveis)
+    const getLogoPath = useCallback((logos: tmdbObjProps[], slideIndex: number) => {
+        if (!slidesData) return;
+
+        if (!logos) {
+            return;
+        };
+
+        const preferredOrder = ['pt', 'pt-BR', 'en'];
+        const logo = preferredOrder.map(lang => logos.find(logo => logo.iso_639_1 === lang)).find(Boolean);
+        const filePath: string | undefined = logo?.file_path;
+        if (!filePath) return;
+        return { path: filePath, slide: slideIndex };
+    }, []);
+
     // recebe o index do slide e atualiza a lista de slides ativos
-    const getActiveSlide = useCallback(( slideInView: number, numberOfSlides: number ): void => {
+    const getActiveSlide = useCallback((slideInView: number, numberOfSlides: number): void => {
         if (!slidesData) return;
         const prev = slideInView === 0 ? numberOfSlides - 1 : slideInView - 1;
         const next = slideInView === numberOfSlides - 1 ? 0 : slideInView + 1;
-        setCurrentSlide({...slidesData[slideInView]});
+        setCurrentSlide({ ...slidesData[slideInView] });
         setActiveSlides([prev, slideInView, next]);
-    }, [ slidesData ]);
+    }, [slidesData]);
+
+    const loadAllLogoImages = useCallback( async () => {
+        if (!slidesData) return;
+
+        const logos = slidesData.map((slide, index) => (getLogoPath(slide.images.logos, index))).filter(logo => logo !== undefined);
+        const resolvedPaths = await Promise.all(logos.map(logo => {
+            return new Promise((resolve, reject) => {
+                const image = new Image();
+                const mediaId: string = slidesData[logo.slide].id;
+                Object.assign(image, {
+                    src: `${ImageBasePath}/w500${logo.path}`,
+                    loading: 'eager',
+                    onload: () => resolve({path: image.src, slide: mediaId}),
+                    onerror: () => reject(`Error ao carregar imagem: ${logo.path}`)
+                });
+            });
+        })) as path[];
+        setLogos([...resolvedPaths]);
+    }, [slidesData, ImageBasePath]);
+
+    useEffect(() => {
+        loadAllLogoImages();
+    }, [slidesData]);
+
+    const getSlideLogoPath = useCallback((slideId: string) => {
+        const logo = logos?.find(logo => logo.slide === slideId);
+        if (!logo?.path) return null;
+        return logo.path;
+    }, [logos]);
 
     return slidesData ? (
         <section className='header-slides'>
-            <EmblaCarousel 
-                navigationType="header" 
-                slidesPerView={1} 
+            <EmblaCarousel
+                navigationType="header"
+                slidesPerView={1}
                 autoplay={true}
                 loop={true}
                 activeIndex={getActiveSlide}>
-            
+
                 {/* Gerando slides apartir da resposta da api do TMDB */}
-                { slidesData.map(( slide, index ) => (
+                {slidesData.map((slide, index) => (
                     // Container da imagem do slide
-                    <Image 
-                        key={`${currentPage}-${slide.id}`} 
+                    <SlideImage
+                        key={`${currentPage}-${slide.id}`}
                         className={`embla__slide ${activeSlides.includes(index) ? 'active-slide' : 'disable-slide'}`}
                         slideData={slide}
                     />
-                ))}  
+                ))}
             </EmblaCarousel>
 
             {/* detalhes do filme/serie */}
-            { currentSlide ? (
+            {currentSlide ? (
                 <div className="absolute left-0 bottom-12 w-full sm:w-fit px-5 flex flex-col items-center justify-between gap-y-4 sm:items-start sm:px-10 sm:bottom-[133px] lg:px-[70px] z-10 pointer-events-none overflow-hidden">
                     {/* titulo */}
-                    <Title title={currentSlide.title ?? currentSlide.name}/>
+                    <Title
+                        slideLogo={getSlideLogoPath(currentSlide.id)}
+                        slideTitle={currentSlide.title ?? currentSlide.name}
+                    />
                     {/* lista com algumas informaçoes sobre filme/serie */}
-                    <DetailsBar slideData={currentSlide}/>
+                    <DetailsBar slideData={currentSlide} />
                     {/* Ir para pagina de detalhes */}
-                    <FurtherDetailsButton onClick={() => navigate(currentSlide.id)} className='mt-2'/>
-                </div>  
-            ) : null }            
+                    <FurtherDetailsButton onClick={() => navigate(currentSlide.id)} className='mt-2' />
+                </div>
+            ) : null}
         </section>
     ) : null;
 };
--- src/components/organisms/headerCarousel/title/index.tsx
+++ src/components/organisms/headerCarousel/title/index.tsx
@@ -1,12 +1,25 @@
-import { useEffect, useRef, memo } from 'react';
+import { useEffect, useRef, memo, useCallback } from 'react';
+import { tmdbConfig } from '@/app/constants';
 import '../styles.css';
 
-const Title = memo(({ title }: { title: string }) => {
+type ComponentProps = {
+    slideLogo: string | null;
+    slideTitle: string;
+};
+
+const Title = memo((props: ComponentProps) => {
     const ref = useRef<HTMLHeadingElement>(null);
-    
+    const imgRef = useRef<HTMLImageElement | null>(null);
+    const titleRef = useRef<HTMLHeadingElement | null>(null);
+    const { slideLogo, slideTitle } = props;
+    const { ImageBasePath } = tmdbConfig;
+    const logoPath = ImageBasePath + '/w500' + slideLogo;
+
     const animation = () => {
-        if ( ref && ref.current ) {
+        if (ref && ref.current) {
             const el = ref.current;
+            const titleText = titleRef.current as HTMLHeadingElement;
+            const titleImage = imgRef.current as HTMLImageElement;
             const animationTime = parseFloat(getComputedStyle(el).animationDuration) * 1000;
 
             Object.assign(el.style, {
@@ -16,11 +29,18 @@ const Title = memo(({ title }: { title: string }) => {
             });
 
             setTimeout(() => {
-                Object.assign(el, {
-                    innerText: title,
-                    animation: 'none',
-                    opacity: 0
-                });
+                Object.assign(el, { animation: 'none', opacity: 0 });
+                if (slideLogo) {
+                    titleImage.src = logoPath;
+                    Object.assign(titleImage.style, { display: 'block', opacity: 0 });
+                    titleText.innerHTML = ''
+                    titleText.style.display = 'none';
+                } else {
+                    titleText.innerHTML = slideTitle;
+                    titleText.style.display = 'inline';
+                    titleImage.src = '';
+                    titleImage.style.display = 'none';
+                };
             }, animationTime + 10);
 
             setTimeout(() => {
@@ -34,11 +54,44 @@ const Title = memo(({ title }: { title: string }) => {
     };
 
     useEffect(() => {
-        animation();
-    }, [title]);
+        if (imgRef.current?.src || titleRef.current?.innerHTML) {
+            animation();
+        } else {
+            if (imgRef && imgRef.current && titleRef && titleRef.current) {
+                if (slideLogo) {
+                    imgRef.current.src = logoPath;
+                    Object.assign(imgRef.current.style, { display: 'block', opacity: 0 });
+                    titleRef.current.innerHTML = '' 
+                    titleRef.current.style.display = 'none';
+                } else {
+                    titleRef.current.innerHTML = slideTitle;
+                    titleRef.current.style.display = 'inline';
+                    imgRef.current.src = '';
+                    imgRef.current.style.display = 'none';
+                };
+            };
+        };
+    }, [slideLogo]);
+
+    const showImage = useCallback(() => {
+        if (!imgRef || !imgRef.current) return;
+        Object.assign(imgRef.current.style, { opacity: '100%' });
+    }, [imgRef]);
 
     return (
-        <h2 ref={ref} className="text-3xl md:duration-300 sm:text-4xl sm:text-start font-black text-secondary text-center line-clamp-1 font-raleway md:text-5xl truncate max-w-8/12 md:leading-14 md:pointer-events-auto sm:max-w-[530px] md:hover:max-w-none"/>  
+        <div ref={ref} className='title-animation-box'>
+            {/* titulo em text caso nenhuma imagem de logo do filme ou series esteja disponivel */}
+            <h2 ref={titleRef} id='title-text' className="text-3xl sm:text-4xl sm:text-start font-black text-secondary text-center line-clamp-1 font-raleway md:text-5xl truncate max-w-8/12 md:leading-14 md:pointer-events-auto sm:max-w-[530px] md:hover:max-w-none " />
+            {/* imagem de logo  */}
+            <img
+                id='title-image'
+                alt={`${slideTitle}'s logo image`}
+                className='max-h-[12vh] max-w-[75vw] h-full sm:max-h-[20vh] sm:max-w-[50vw] md:max-w-[40vw] md:max-h-[25vh] lg:max-h-[30vh] xl:max-w-[35vw] 2xl:max-h-[40vh] 2xl:max-w-[40vw] w-fit' loading='eager'
+                ref={imgRef}
+                style={{ filter: 'brightness(170%)' }}
+                onLoad={showImage}
+            />
+        </div>
     );
 });
 
