025a5874da9e4a6e86796b058c22be2c650ae58e
Vitor Hugo <Hugosilva55442@gmail.com>
2025-08-01 08:51:13 -0300

    fix(play-icon): correct attribute definition error

--- src/components/atoms/playIcon/index.tsx
+++ src/components/atoms/playIcon/index.tsx
@@ -10,7 +10,7 @@ export const PlayIcon = (props: ComponentProps) => {
             <desc>
                 Play Streamline Icon: https://streamlinehq.com
             </desc>
-            <path d="M21.4086 9.35258c2.1219 1.15392 2.1219 4.14092 0 5.29482L8.59662 21.6145C6.53435 22.736 4 21.2763 4 18.9671L4 5.0329c0 -2.30922 2.53435 -3.76888 4.59661 -2.64742l12.81199 6.9671Z" fill="currentColor" stroke-width="1"></path>
+            <path d="M21.4086 9.35258c2.1219 1.15392 2.1219 4.14092 0 5.29482L8.59662 21.6145C6.53435 22.736 4 21.2763 4 18.9671L4 5.0329c0 -2.30922 2.53435 -3.76888 4.59661 -2.64742l12.81199 6.9671Z" fill="currentColor" strokeWidth="1"></path>
         </svg>
     )
 };
