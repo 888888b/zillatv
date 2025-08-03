ae59366b51e840a6a8ea0134e531f86ee7965ad7
Vitor Hugo <Hugosilva55442@gmail.com>
2025-08-03 17:43:23 -0300

    feat(section-title): add className prop for defining classes in parent components

--- src/components/templates/playerPage/sectionTitle/index.tsx
+++ src/components/templates/playerPage/sectionTitle/index.tsx
@@ -1,5 +1,14 @@
-export const SectionTitle = ({children}:{children: string}) => {
+import { ReactNode } from "react";
+
+type ComponentProps = {
+    children: ReactNode;
+    className?: string;
+};
+
+export const SectionTitle = (props: ComponentProps) => {
+    const {children, className} = props;
+
     return (
-        <h2 className='text-[19px] leading-6 font-black font-raleway md:text-[22px] md:leading-7 tracking-wide text-secondary justify-between md:justify-start gap-x-[10px] text-center sm:text-left cursor-pointer transition-colors duration-300 w-full sm:w-fit '>{children}</h2>
+        <h2 className={`text-[19px] leading-6 font-black font-raleway md:text-[22px] md:leading-7 tracking-wide text-secondary cursor-pointer transition-colors duration-300 w-full sm:w-fit ${className}`}>{children}</h2>
     );
 };
