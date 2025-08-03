34c28a62d8919f1270d1b05931ecfbb47a30ef7f
Vitor Hugo <Hugosilva55442@gmail.com>
2025-08-03 17:45:20 -0300

    refactor(utils-tmdb): refactor date formatting function by adding logic to prevent null input values

--- src/components/utils/tmdbApiData/releaseDate/index.ts
+++ src/components/utils/tmdbApiData/releaseDate/index.ts
@@ -1,5 +1,6 @@
 /*Função que obtem o ano de lançamento de um filme ou serie*/
 export const getReleaseDate = ( date: string ): string => {
+    if (!date) return '';
     const newDate = [];
     for ( let i = 0; i < 4; i++ ) {
         newDate.push( date[i] );
