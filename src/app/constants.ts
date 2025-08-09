d2bddc145682c77d742522ca4e359134ebe92d20
Vitor Hugo <Hugosilva55442@gmail.com>
2025-08-09 17:01:52 -0300

    feat(tmdb-config): add new genres for series and movies

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -15,29 +15,48 @@ export const tmdbConfig = {
 export const tmdbGenres = {
   cartoon: { genre: '16', title: 'Desenho' },
   release: { genre: 'release', title: 'Lançamentos' },
-  trending: { genre:'trending', title: 'Em alta' },
+  trending: { genre: 'trending', title: 'Em alta' },
   horror: { genre: '27', title: 'Terror' },
   action: { genre: '28', title: 'Ação' },
   comedy: { genre: '35', title: 'Comédia' },
   romance: { genre: '10749', title: 'Romance' },
-  documentary: { genre: '99', title: 'Documentario' }, 
+  documentary: { genre: '99', title: 'Documentário' },
   war: { genre: '10752', title: 'Guerra' },
   fiction: { genre: '878', title: 'Ficção' },
   adventure: { genre: '12', title: 'Aventura' },
-  popular: { genre:'popular', title: 'Populares' }
+  popular: { genre: 'popular', title: 'Populares' },
+  crime: { genre: '80', title: 'Crime' },
+  drama: { genre: '18', title: 'Drama' },
+  family: { genre: '10751', title: 'Família' },
+  history: { genre: '36', title: 'História' },
+  music: { genre: '10402', title: 'Música' },
+  mystery: { genre: '9648', title: 'Suspense' },
+  tvmovie: { genre: '10770', title: 'TV' },
+  thriller: { genre: '53', title: 'Thriller' },
+  western: { genre: '37', title: 'Faroeste' }
 };
 
 // generos e respectivos titulos para series
 export const tmdbSerieGenres = {
-  release: { genre: 'release', title: 'Lançamentos', },
-  suspense: { genre: '9648', title: 'Suspense', },
-  trending: { genre:'trending', title: 'Em alta' },
-  action: { genre: '10759', title: 'Ação', },
-  comedy: { genre: '35', title: 'Comedia', },
-  war: { genre: '10768', title: 'Guerra', },
-  cartoon: { genre: '16', title: 'Desenho', },
-  documentary: { genre: '99', title: 'Documentario', },
-  popular: { genre:'popular', title: 'Populares' }
+  release: { genre: 'release', title: 'Lançamentos' },
+  suspense: { genre: '9648', title: 'Suspense' },
+  trending: { genre: 'trending', title: 'Em alta' },
+  action: { genre: '10759', title: 'Ação' },
+  comedy: { genre: '35', title: 'Comédia' },
+  war: { genre: '10768', title: 'Guerra' },
+  cartoon: { genre: '16', title: 'Desenho' },
+  documentary: { genre: '99', title: 'Documentário' },
+  popular: { genre: 'popular', title: 'Populares' },
+  crime: { genre: '80', title: 'Crime' },
+  drama: { genre: '18', title: 'Drama' },
+  family: { genre: '10751', title: 'Família' },
+  kids: { genre: '10762', title: 'Infantil' },
+  news: { genre: '10763', title: 'Notícias' },
+  reality: { genre: '10764', title: 'Reality' },
+  scifi: { genre: '10765', title: 'Ficção' },
+  soap: { genre: '10766', title: 'Novela' },
+  talk: { genre: '10767', title: 'Entrevista' },
+  western: { genre: '37', title: 'Faroeste' }
 };
 
 
