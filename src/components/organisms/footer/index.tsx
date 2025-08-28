a8510318125d71cc4eb79000836bf706ac34c263
Vitor Hugo <Hugosilva55442@gmail.com>
2025-08-28 12:08:36 -0300

    style(footer): apply new design

--- src/components/organisms/footer/index.tsx
+++ src/components/organisms/footer/index.tsx
@@ -1,106 +1,68 @@
+import { AndroidIcon } from '@/components/atoms/androidIcon';
+import { WindowsIcon } from '@/components/atoms/windowsIcon';
 import './styles.css';
 
-type ComponentProps = {className?: string;};
+type ComponentProps = { className?: string; };
 
 export default function Footer(props: ComponentProps) {
-    const {className} = props;
+    const { className } = props;
 
     return (
-        <footer className={`px-4 sm:px-10 lg:px-[70px] py-14 bg-surface ${className}`}>
-            <h2 className="text-primary font-black text-[22px] font-raleway">ZilluTV</h2>
-            <div className="bg-secondary/5 h-px w-full max-w-48 mt-[15px]" />
+        <footer className={`px-4 sm:px-10 lg:px-16 pt-16 pb-8 bg-surface border-t-2 border-secondary/5 flex flex-col gap-y-16 items-center ${className}`}>
+            {/* titulo */}
+            <h2 className="w-fit font-semibold text-lg uppercase text-secondary hidden md:inline">
+                Filmes populares - séries em alta - animes online
+            </h2>
 
-            <div className="flex flex-wrap gap-y-20 gap-x-[60px] md:gap-20 items-start lg:justify-between mt-10">
-                <div>
-                    <h3 className="font-bold uppercase text-base text-secondary">Conta</h3>
-                    <div className="mt-5">
-                        <ul className="font-normal flex flex-col gap-y-[10px] text-base">
-                            <li><a href="">Criar conta</a></li>
-                            <li><a href="">Entrar</a></li>
-                            <li><a href="">Assinatura</a></li>
-                            <li><a href="">Dispositivos compatíveis</a></li>
-                        </ul>
-                    </div>
+            <div className='custom-grid flex flex-col gap-y-16'>
+                {/* downloads */}
+                <div className='column flex flex-col'>
+                    <h3>Baixe nosso aplicativo</h3>
+                    {/* baixar aplicativo android */}
+                    <button className='mt-6 flex items-center gap-x-2 transition-transform active:scale-95 duration-200'>
+                        <AndroidIcon size={20}/>
+                        Android
+                    </button>
+                    {/* baixar aplicativo desktop*/}
+                    <button className='mt-2 flex items-center gap-x-2 transition-transform active:scale-95 duration-200'>
+                        <WindowsIcon size={20}/>
+                        Windows
+                    </button>
+                    <p className='mt-4'>
+                        Tenha acesso à nossa plataforma de filmes no seu celular Android ou computador!
+                    </p>
                 </div>
 
-                <div>
-                    <h3 className="font-bold uppercase text-base text-secondary">Sobre</h3>
-                    <div className="mt-5">
-                        <ul className="font-normal flex flex-col gap-y-[10px] text-base">
-                            <li><a href="">Quem Somos</a></li>
-                            <li><a href="">Parceiros</a></li>
-                            <li><a href="">Trabalhe conosco</a></li>
-                        </ul>
-                    </div>
+                {/* sobre conteudos e api */}
+                <div className='column flex flex-col'>
+                    <h3>Conteúdos & API</h3> 
+                    <img
+                        src='/tmdb_logo.png'
+                        alt='Imagem logo do TMDB'
+                        loading='lazy'
+                        className='h-9 w-fit mt-6'
+                    />
+                    <p className='mt-3'>
+                        As informações sobre filmes e séries exibidas nesta plataforma são fornecidas pela API do The Movie Database (TMDB). Todos os direitos autorais pertencem aos seus respectivos detentores.
+                    </p>
                 </div>
 
-                <div>
-                    <h3 className="font-bold uppercase text-base text-secondary">Ajuda</h3>
-                    <div className="mt-5">
-                        <ul className="font-normal flex flex-col gap-y-[10px] text-base">
-                            <li><a href="">Central de ajuda</a></li>
-                            <li><a href="">Fale conosco</a></li>
-                            <li><a href="">Status do serviço</a></li>
-                            <li><a href="">Perguntas frequentes</a></li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div>
-                    <h3 className="font-bold uppercase text-base text-secondary">Legal</h3>
-                    <div className="mt-5">
-                        <ul className="font-normal flex flex-col gap-y-[10px] text-base">
-                            <li><a href="">Termos de uso</a></li>
-                            <li><a href="">Política de Privacidade</a></li>
-                            <li><a href="">Política de Cookies</a></li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div>
-                    <h3 className="font-bold uppercase text-base text-secondary">Nossas redes</h3>
-                    <div className="mt-5">
-                        <ul className="font-normal flex flex-col gap-y-[10px] text-base">
-                            <li><a href="">Instagram</a></li>
-                            <li><a href="">Twitter/X</a></li>
-                            <li><a href="">Facebook</a></li>
-                            <li><a href="">YouTube</a></li>
-                        </ul>
-                    </div>
+                {/* breve descrição sobre o projeto */}
+                <div className='column flex flex-col'>
+                    <h3>Sobre o projeto</h3> 
+                    <img
+                        src='/project_logo.svg'
+                        alt='Imagem logo do TMDB'
+                        loading='lazy'
+                        className='h-9 w-fit mt-6'
+                    />
+                    <p className='mt-3'>
+                        Somos uma plataforma dedicada a reunir informações sobre filmes e séries. Nosso objetivo é oferecer detalhes, sinopses e dados organizados de forma simples e acessível, ajudando você a descobrir e acompanhar suas produções favoritas.
+                    </p>
                 </div>
             </div>
 
-            <div className="bg-secondary/5 h-px w-full my-10" />
-
-            <div className="flex items-center flex-col gap-10">
-                <div className='flex flex-wrap gap-5 justify-center items-center'>
-                    {/* icone do google play */}
-                    <img
-                        src={'/google_play_badge.png'}
-                        alt='Google Play badge'
-                        className='h-10 w-auto cursor-pointer'
-                        loading='lazy'
-                    />
-
-                    {/* icon da apple store */}
-                    <img
-                        src={'/apple_store_badge.png'}
-                        alt='Apple store badge'
-                        className='h-11 w-auto cursor-pointer'
-                        loading='lazy'
-                    />
-
-                     {/* icon da microsoft store */}
-                    <img
-                        src={'/microsoft_store_badge.svg'}
-                        alt='Microsoft store badge'
-                        className='h-10 w-auto cursor-pointer border border-secondary/65 rounded-md'
-                        loading='lazy'
-                    />
-                </div>
-
-                <p className="text-base font-semibold">© 2025 ZilluTV - Todos os direitos reservados</p>
-            </div>
+            <p className='text-center'>© 2025 ZillaTV. Todos os direitos reservados.</p>
         </footer>
     );
 };
