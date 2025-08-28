e80af9e0b566f7dbe7107a80b931985bedb935b1
Vitor Hugo <Hugosilva55442@gmail.com>
2025-08-28 12:10:32 -0300

    feat:add icons (android, windows) to the footer

---
+++ src/components/atoms/windowsIcon/index.tsx
@@ -0,0 +1,14 @@
+type ComponentProps = {
+    size: number;
+};
+
+export const WindowsIcon = (props: ComponentProps) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-windows" viewBox="0 0 16 16" id="Windows--Streamline-Bootstrap" height={props.size} width={props.size}>
+            <desc>
+                Windows Streamline Icon: https://streamlinehq.com
+            </desc>
+            <path d="M6.555 1.375 0 2.237v5.45h6.555zM0 13.795l6.555 0.933V8.313H0zm7.278 -5.4 0.026 6.378L16 16V8.395zM16 0 7.33 1.244v6.414H16z" strokeWidth="1"></path>
+        </svg>
+    );
+};
