7ad03564683a50c39b9487d2fda1d99dd20f4a6f
Vitor Hugo <Hugosilva55442@gmail.com>
2025-09-07 09:39:15 -0300

    feat(tmdb/constants): add genres and their respective titles

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -17,18 +17,23 @@ export const tmdbGenres = {
   release: { genre: 'release', title: 'Lançamentos' },
   trending: { genre: 'trending', title: 'Em alta' },
   horror: { genre: '27', title: 'Terror' },
+  netflix: { genre: 'netflix', title: 'Netflix'},
   action: { genre: '28', title: 'Ação' },
   comedy: { genre: '35', title: 'Comédia' },
   romance: { genre: '10749', title: 'Romance' },
+  HBO: { genre: 'hbo', title: 'HBO Max'},
   documentary: { genre: '99', title: 'Documentário' },
   war: { genre: '10752', title: 'Guerra' },
+  disney: { genre: 'disney', title: 'Disney Plus'},
   fiction: { genre: '878', title: 'Ficção' },
   adventure: { genre: '12', title: 'Aventura' },
   popular: { genre: 'popular', title: 'Populares' },
+  hulu: { genre: 'hulu', title: 'Hulu'},
   crime: { genre: '80', title: 'Crime' },
   drama: { genre: '18', title: 'Drama' },
   family: { genre: '10751', title: 'Família' },
   history: { genre: '36', title: 'História' },
+  prime: { genre: 'prime', title: 'Prime Video'},
   music: { genre: '10402', title: 'Música' },
   mystery: { genre: '9648', title: 'Suspense' },
   tvmovie: { genre: '10770', title: 'TV' },
@@ -42,22 +47,47 @@ export const tmdbSerieGenres = {
   suspense: { genre: '9648', title: 'Suspense' },
   trending: { genre: 'trending', title: 'Em alta' },
   action: { genre: '10759', title: 'Ação' },
+  netflix: { genre: 'netflix', title: 'Netflix'},
   comedy: { genre: '35', title: 'Comédia' },
   war: { genre: '10768', title: 'Guerra' },
   cartoon: { genre: '16', title: 'Desenho' },
   documentary: { genre: '99', title: 'Documentário' },
+  HBO: { genre: 'hbo', title: 'HBO Max'},
   popular: { genre: 'popular', title: 'Populares' },
   crime: { genre: '80', title: 'Crime' },
+  disney: { genre: 'disney', title: 'Disney Plus'},
   drama: { genre: '18', title: 'Drama' },
   family: { genre: '10751', title: 'Família' },
   kids: { genre: '10762', title: 'Infantil' },
+  hulu: { genre: 'hulu', title: 'Hulu'},
   news: { genre: '10763', title: 'Notícias' },
   reality: { genre: '10764', title: 'Reality' },
   scifi: { genre: '10765', title: 'Ficção' },
   soap: { genre: '10766', title: 'Novela' },
   talk: { genre: '10767', title: 'Entrevista' },
+  prime: { genre: 'prime', title: 'Prime Video'},
   western: { genre: '37', title: 'Faroeste' }
 };
 
+export const headerMoviesList = [
+  "1233413",  // Pecadores
+  "438631",   // Duna 1
+  "650031",  // O Bombardeio
+  "157336",  // Interstellar
+  "524434",  // Eternos
+  "603692",  // John Wick capitulo 4
+  "12153",     // As branquelas
+  "696806",  // O Projeto Adam
+];
 
+export const headerSeriesList = [
+  "100088",  // The last of us
+  "66732",   // Stranger Things
+  "87108",   // Chernobyl
+  "76479",   // The Boys
+  "1399",    // Game of Thrones
+  "1396",    // Breaking Bad
+  "82856",   // The Mandalorian
+  "94997"   // House of the Dragon
+];
 
