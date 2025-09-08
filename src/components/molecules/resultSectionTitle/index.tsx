e1435d3956e3f3f16ea210f6fd6d8faa194a6b9f
Vitor Hugo <Hugosilva55442@gmail.com>
2025-09-08 13:15:12 -0300

    style(search-page/title): change line height

--- src/components/molecules/resultSectionTitle/index.tsx
+++ src/components/molecules/resultSectionTitle/index.tsx
@@ -14,7 +14,7 @@ export default function children( props: ComponentProps ) {
                     {children ? 'Exibindo resultados para' : 'Exibindo'}
                 </p>
 
-                <h2 className=" leading-9 text-secondary font-black text-4xl md:leading-11 xl:text-5xl xl:leading-[60px] line-clamp-3 overflow-ellipsis max-w-[768px]">
+                <h2 className="text-secondary font-black text-4xl leading-11 xl:text-5xl xl:leading-[60px] line-clamp-3 overflow-ellipsis max-w-[768px]">
                     {children ? 
                     children 
                     : 
