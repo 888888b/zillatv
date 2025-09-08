767869218b3f0c7e26c39e0dc7e3fc2cc9ae323f
Vitor Hugo <Hugosilva55442@gmail.com>
2025-09-08 07:49:19 -0300

    feat(fetchMultiPerRequest): change request to respond with images in Portuguese if available

--- src/hooks/tmdb/multiTypesPerRequest/index.ts
+++ src/hooks/tmdb/multiTypesPerRequest/index.ts
@@ -4,7 +4,7 @@ export const fetchMultiTypes = async ( keyword: string ): Promise<tmdbObjProps[]
     const api_key = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 
     try {
-        const response = await fetch(`https://api.themoviedb.org/3/search/multi?query=${keyword}&api_key=${api_key}&include_adult=false&language=en-US&page=1`);
+        const response = await fetch(`https://api.themoviedb.org/3/search/multi?query=${keyword}&api_key=${api_key}&include_adult=false&language=pt-BR&include_image_language=pt,null&page=1`);
         if ( response.ok ) {
             const data = await response.json();
             return data.results;
