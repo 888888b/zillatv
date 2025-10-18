698a6904172ede70ee378f4fe1a95e85cd28e3f0
Vitor Hugo <Hugosilva55442@gmail.com>
2025-10-18 18:00:27 -0300

    style(hero-play-button): increase horizontal gap from 3px to 6px

--- src/components/atoms/playButton/index.tsx
+++ src/components/atoms/playButton/index.tsx
@@ -5,7 +5,7 @@ const PlayButton = memo(( props: ComponentPropsWithRef<'button'> ) => {
     return (
         <button
             {...rest}
-            className={`[font-size:clamp(1rem,1.15vw,1.125rem)] h-[clamp(2.5rem,4.7vw,3rem)] pointer-events-auto outline-none border-none w-fit rounded-md bg-primary text-background font-bold px-[1.5em] active:scale-95 transition-transform duration-200 flex items-center gap-x-3 justify-center cursor-pointer ${className}`}>
+            className={`[font-size:clamp(1rem,1.15vw,1.125rem)] h-[clamp(2.5rem,4.7vw,3rem)] pointer-events-auto outline-none border-none w-fit rounded-md bg-primary text-background font-bold px-[1.5em] active:scale-95 transition-transform duration-200 flex items-center gap-[6px] justify-center cursor-pointer ${className}`}>
             {children}
         </button>
     );
