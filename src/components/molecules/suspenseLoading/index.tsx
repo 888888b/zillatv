8d46eee7bfb83a133d8839a66aa743ca7f749d82
Vitor Hugo <Hugosilva55442@gmail.com>
2025-11-01 09:26:22 -0300

    chore: delete unnecessary components

--- src/components/molecules/suspenseLoading/index.tsx
+++
@@ -1,7 +0,0 @@
-export const SuspenseLoading = () => {
-    return (
-        <div className="w-screen h-dvh fixed top-0 left-0 z-[999] bg-surface flex items-center justify-center">
-            <span className="loading loading-dots loading-lg bg-secondary/80"></span>
-        </div>
-    );
-};
