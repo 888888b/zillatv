b3236423b8d93736a4895c96a92a7c1986779f18
Vitor Hugo <Hugosilva55442@gmail.com>
2025-11-07 17:10:16 -0300

    feat(lang-selector): adds overlay when opening selector, fixes unexpected behaviors, adds translate to element when clicking

--- src/components/molecules/langSelector/index.tsx
+++ src/components/molecules/langSelector/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 // hooks
-import { useState, useRef } from 'react';
+import { useState, useRef, MouseEvent, useCallback } from 'react';
 // icones
 import { BsFillPlayFill } from "react-icons/bs";
 import { RiGlobalLine } from "react-icons/ri";
@@ -10,12 +10,19 @@ import useLanguage from '@/hooks/lang';
 import { languages } from '@/i18n/languages';
 // tipo
 import { Lang } from '@/contexts/lang';
+// estilos
+import './styles.css';
 
 export default function LangSelector() {
     const [isOpened, setIsOpened] = useState(false);
     const btnRef = useRef<HTMLButtonElement | null>(null);
-    const toggleSelect = (): void => setIsOpened(prev => !prev);
+    const langsListRef = useRef<HTMLDivElement | null>(null);
     const { language, setLanguage } = useLanguage();
+
+    const toggleSelect = useCallback((): void => {
+        setIsOpened(prev => !prev);
+        if (langsListRef.current) langsListRef.current.scrollIntoView({behavior: 'smooth'});
+    }, [langsListRef]);
 
     const setNewLang = (lang: Lang) => {
         setLanguage(lang);
@@ -23,26 +30,30 @@ export default function LangSelector() {
     };
 
     return (
-        <div className="relativ w-fit">
+        <div className="w-fit lang-selector">
+            {/* overlay  */}
             {/* botao do select */}
-            <button className={`outline-0 h-[clamp(40px,4.8vw,48px)] rounded-md border-2 px-4 flex items-center font-semibold gap-x-2 [font-size:clamp(0.875rem,1vw,1rem)] cursor-pointer  duration-300 transition-colors ${isOpened ? 'rounded-t-none text-secondary bg-surface border-secondary/5 border-t-0' : 'border-secondary/30 hover:text-secondary hover:border-secondary'}`} onClick={toggleSelect} ref={btnRef}>
+            <button id='lang-selector' className={`relative outline-0 min-w-[170px] h-[clamp(40px,4.8vw,48px)] rounded-md border-2 px-4 flex items-center font-semibold gap-x-2 justify-between [font-size:clamp(0.875rem,1vw,1rem)] cursor-pointer  duration-100 transition-all ${isOpened ? 'rounded-t-none text-secondary bg-surface border-secondary/5 border-t-transparent z-[101]' : 'border-secondary/30 hover:text-secondary hover:border-secondary'}`} onClick={toggleSelect} ref={btnRef}>
                 <RiGlobalLine className="[font-size:clamp(22px,1.5vw,24px)]" />
                 {language.label}
                 <BsFillPlayFill className="rotate-90" />
             </button>
             {/* opções do select */}
-            {(isOpened && btnRef.current) &&
-                <div className='absolute left-[var(--page-padding)] -translate-y-[calc(100%+clamp(40px,4.8vw,48px))] bg-surface rounded-t-md before:absolute before:-bottom-0.5 before:h-5 before:w-full before:bg-gradient-to-b before:from-transparent before:to-surface before:z-10 border-2 border-secondary/5' style={{ width: btnRef.current.offsetWidth }}>
-                    <ul className="max-h-[70vh] overflow-y-scroll *:hover:underline *:px-4 *:py-3 py-3 *:cursor-pointer font-medium text-sm">
-                        {languages.map(lang => (
-                            <li 
-                                onClick={() => setNewLang(lang)}
-                                key={lang.code} 
-                                className={`${lang.code === language.code && 'text-secondary font-semibold'}`}>
-                                {lang.label}
-                            </li>
-                        ))}
-                    </ul>
+            {(btnRef.current) &&
+                <div className={isOpened ? 'opened-selector' : 'closed-selector'}>
+                    <div className='overlay fixed top-0 left-0 bg-background/80 w-full h-full z-[100]' />
+                    <div id='languages' ref={langsListRef} className='languages-list z-[101] absolute -translate-y-[calc(100%+clamp(40px,4.8vw,48px))] bg-surface rounded-t-md before:absolute before:-bottom-0.5 before:h-5 before:w-full before:bg-gradient-to-b before:from-transparent before:to-surface before:z-10 border-2 border-secondary/5' style={{ width: btnRef.current.offsetWidth }}>
+                        <ul className="max-h-[70vh] overflow-y-scroll *:hover:underline *:px-4 *:py-3 py-3 *:cursor-pointer font-medium text-sm">
+                            {languages.map(lang => (
+                                <li
+                                    onClick={() => setNewLang(lang)}
+                                    key={lang.code}
+                                    className={`${lang.code === language.code && 'text-secondary font-semibold'}`}>
+                                    {lang.label}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
             }
         </div>
