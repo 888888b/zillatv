def64464802353b64767b9963dfeacf2843c82c6
Vitor Hugo <Hugosilva55442@gmail.com>
2025-11-07 18:12:56 -0300

    feat(genre-selector): add overlay when opening selector

--- src/components/molecules/genreSelect/index.tsx
+++ src/components/molecules/genreSelect/index.tsx
@@ -13,10 +13,12 @@ type ComponentProps = {
     selectedGenre: GenreType;
     genres: TmdbMediaProps;
 };
+// estilos
+import './styles.css';
 
 const GenreSelect = memo(({ genres, onSelectGenre, selectedGenre }: ComponentProps) => {
     const ref = useRef<HTMLButtonElement | null>(null);
-    const [open, setOpen] = useState(false);
+    const [isOpened, setIsOpened] = useState(false);
     const lang = useLanguage().language.code;
     const text = translations[lang];
 
@@ -24,7 +26,7 @@ const GenreSelect = memo(({ genres, onSelectGenre, selectedGenre }: ComponentPro
         const el = ref.current;
         if (!el) return;
         const { top } = el.getBoundingClientRect();
-        setOpen(prev => !prev);
+        setIsOpened(prev => !prev);
         window.scrollTo({
             top: top + window.scrollY - 150,
             behavior: "smooth",
@@ -33,12 +35,12 @@ const GenreSelect = memo(({ genres, onSelectGenre, selectedGenre }: ComponentPro
 
     const handleSelectGenre = useCallback((genre: string[]) => {
         if (selectedGenre.genre === genre[0]) return;
-        onSelectGenre({genre: genre[0], title: genre[1]});
-        setOpen(false);
-    },[onSelectGenre, selectedGenre]);
+        onSelectGenre({ genre: genre[0], title: genre[1] });
+        setIsOpened(false);
+    }, [onSelectGenre, selectedGenre]);
 
     return (
-        <div className="relative">
+        <div className={`relative selector-wrapper ${isOpened ? 'opened' : 'closed'}`}>
             <h3 className="font-medium mb-1 text-center sm:text-left [font-size:clamp(1rem,1.15vw,1.125rem)]">
                 {text.genre}
             </h3>
@@ -46,11 +48,8 @@ const GenreSelect = memo(({ genres, onSelectGenre, selectedGenre }: ComponentPro
             {/* Seletor */}
             <button
                 ref={ref}
-                id="genre-selector"
                 onClick={toggleDropdown}
-                className={`border border-secondary/20 rounded-md flex items-center gap-8 cursor-pointer md:hover:border-secondary transition-all duration-300 text-secondary font-medium w-fit [font-size:clamp(1.125rem,1.3vw,1.25rem)] h-12 pl-6 pr-2 overflow-hidden mx-auto sm:mx-0 ${open ? "z-50" : ""
-                    }`}
-            >
+                className={`selector border border-secondary/20 rounded-md flex items-center gap-8 cursor-pointer md:hover:border-secondary transition-all duration-300 text-secondary font-medium w-fit [font-size:clamp(1.125rem,1.3vw,1.25rem)] h-12 pl-6 pr-2 overflow-hidden mx-auto sm:mx-0`}>
                 <span className="min-w-[150px] text-left pointer-events-none">
                     {selectedGenre.title}
                 </span>
@@ -61,22 +60,23 @@ const GenreSelect = memo(({ genres, onSelectGenre, selectedGenre }: ComponentPro
                         width={24}
                         height={24}
                         stroke="2"
-                        className={`${open ? "-rotate-90 text-text" : "rotate-90 text-secondary"}`}
+                        className={'arrow-right'}
                     />
                 </div>
             </button>
-
+            {/* overlay */}
+            <div className='overlay fixed top-0 left-0 bg-background/80 w-full h-[100lvh] z-[100]' />
             {/* Dropdown */}
             <div
-                className={`absolute left-0 top-[86px] w-full sm:w-fit max-w-[768px] md:max-w-[80vw] lg:max-w-[60vw] bg-surface border-2 border-secondary/5 rounded-md px-6 py-5 ${open ? "visible z-40" : "invisible"}`}>
-                <ul className="grid grid-cols-[auto] justify-items-center sm:grid-cols-[auto_auto_auto] md:grid-cols-[auto_auto_auto_auto] gap-8 sm:gap-y-5 sm:gap-x-10 max-h-[55vh] overflow-scroll [font-size:clamp(1.0625rem,1.2vw,1.1875rem)] *:cursor-pointer *:hover:underline">
+                className={`selector-options absolute left-0 top-[86px] w-full sm:w-fit max-w-[768px] md:max-w-[80vw] lg:max-w-[60vw] bg-surface border-2 border-secondary/5 rounded-md px-6 py-8 z-[101]`}>
+                <ul className="grid grid-cols-[auto] justify-items-center sm:grid-cols-[auto_auto_auto] md:grid-cols-[auto_auto_auto_auto] gap-8 sm:gap-y-5 sm:gap-x-10 max-h-[50vh] overflow-y-scroll [font-size:clamp(1.0625rem,1.2vw,1.1875rem)] *:cursor-pointer *:hover:underline">
                     {Object.entries(genres).map((genre) => (
                         // index 0 === genero / index 1 === titulo a ser exibido
                         <li
                             key={`${genre[0]}-movie-genre`}
                             onClick={() => handleSelectGenre(genre)}
-                            className={ selectedGenre.genre === genre[0]
-                                    ? "text-primary font-semibold pointer-events-none" : ""}>
+                            className={selectedGenre.genre === genre[0]
+                                ? "text-primary font-semibold pointer-events-none" : ""}>
                             {genre[1]}
                         </li>
                     ))}
