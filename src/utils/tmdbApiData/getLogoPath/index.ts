ad251fe7e8dcc703e91fcbfd1ea4a3147f14b818
Vitor Hugo <Hugosilva55442@gmail.com>
2025-11-08 18:55:27 -0300

    feat(getLogoPath): adds "lang" parameter and filters logos by current page language.

--- src/utils/tmdbApiData/getLogoPath/index.ts
+++ src/utils/tmdbApiData/getLogoPath/index.ts
@@ -2,10 +2,14 @@ import { TmdbMediaProps } from '@/app/types';
 export type Path = { path: string, slide: string };
 
 // filtra a lista de imagens de logo buscando por uma imagem em portugues (caso houver logos disponiveis)
-export const getLogoPath = (logos: TmdbMediaProps[], slideId: string): Path | undefined => {
+export const getLogoPath = (
+    logos: TmdbMediaProps[], 
+    slideId: string,
+    lang: string = 'pt'
+): Path | undefined => {
     if (!Array.isArray(logos) || !logos) return;
-    const preferredOrder = ['pt', 'pt-BR', 'en'];
-    const logo = preferredOrder.map(lang => logos.find(logo => logo.iso_639_1 === lang)).find(Boolean);
+    const languages = [lang, 'en', 'null'];
+    const logo = languages.map(lang => logos.find(logo => logo.iso_639_1 === lang)).find(Boolean);
     const filePath: string | undefined = logo?.file_path;
     if (!filePath) return;
     return { path: filePath, slide: slideId };
