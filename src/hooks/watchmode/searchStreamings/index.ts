f6ab25658ebe8b2d8f2b9feb281e9adf4eec7255
Vitor Hugo <Hugosilva55442@gmail.com>
2025-11-10 20:24:42 -0300

    feat(watchmode): set BR and US as default regions for streaming search

--- src/hooks/watchmode/searchStreamings/index.ts
+++ src/hooks/watchmode/searchStreamings/index.ts
@@ -1,9 +1,12 @@
 import { StreamingsInfo } from "..";
 
-export const searchStreamingsById = async (id: string): Promise<StreamingsInfo[] | undefined> => {
+export const searchStreamingsById = async (
+    id: string, lang: string = 'pt-BR'): Promise<StreamingsInfo[] | undefined> => {
     const token = process.env.WATCH_MODE_API_KEY;
+    const region = lang.split('-')[1];
+
     try {
-        const response = await fetch(`https://api.watchmode.com/v1/title/${id}/sources/?apiKey=${token}&regions=BR`, {
+        const response = await fetch(`https://api.watchmode.com/v1/title/${id}/sources/?apiKey=${token}&regions=${region === 'BR' ? region : 'US'}`, {
             cache: 'force-cache',
             next: { revalidate: 3600 }
         });
@@ -11,7 +14,8 @@ export const searchStreamingsById = async (id: string): Promise<StreamingsInfo[]
             const data = await response.json();
             return data;
         };
-    } catch (error) {
-        console.error('Watchmode: Erro ao buscar streamings.' + error);
+
+    } catch (err) {
+        console.error('Watchmode: Erro ao buscar streamings.' + err);
     };
 };
